91d65428f7af27a29c8323e09046c5f00aed226b
Wayne Tng <wayne@futureworkz.com>
2016-05-24 20:28:45 +0800

    Add image validation to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ActiveRecord::Base
   validates :name,  presence: true
   validates :sku,   presence: true, uniqueness: true
   validates :price, presence: true, numericality: true
+
+  has_attached_file :image, styles: { medium: '300x300', thumb: '100x100' }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: ["image/jpg",
+                                                            "image/png", 
+                                                            "image/jpeg", 
+                                                            "image/gif", 
+                                                            "image/pjpeg", 
+                                                            "image/x-png"]
+
 end
