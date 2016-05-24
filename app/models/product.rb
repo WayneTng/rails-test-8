8b6240aabe9c98efc7e09cac1376d1f872fa8f3b
Wayne Tng <wayne@futureworkz.com>
2016-05-24 20:34:26 +0800

    Change validation for price in Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   validates :name,  presence: true
   validates :sku,   presence: true, uniqueness: true
-  validates :price, presence: true, numericality: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 
   has_attached_file :image, styles: { medium: '300x300', thumb: '100x100' }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: ["image/jpg",
@@ -10,5 +10,4 @@ class Product < ActiveRecord::Base
                                                             "image/gif", 
                                                             "image/pjpeg", 
                                                             "image/x-png"]
-
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   context 'Validation' do
     context 'Presence' do
-      it { is_expected.to validate_presence_of :sku  }
-      it { is_expected.to validate_presence_of :name  }
-      it { is_expected.to validate_presence_of :price  }
+      it { is_expected.to validate_presence_of :sku }
+      it { is_expected.to validate_presence_of :name }
+      it { is_expected.to validate_presence_of :price }
     end
 
     context 'Numericality' do
-      it { is_expected.to validate_numericality_of :price }
+      it { is_expected.to validate_numericality_of(:price).is_greater_than 0 }
     end
 
     context 'Uniqueness' do
